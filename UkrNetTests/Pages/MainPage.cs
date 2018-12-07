cda51effd30ea03ff42be7d78f017d9370980279
vadd3012 <vadd3012@gmail.com>
2018-12-07 09:31:25 +0200

    Add search test

---
+++ UkrNetTests/Pages/MainPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace UkrNetTests.Pages
+{
+    public class MainPage
+    {
+        public MainPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);            
+        }
+        [FindsBy(How = How.CssSelector, Using = "input[id='id-input-login'][class='form__input']")]
+        public IWebElement loginInput;
+
+        [FindsBy(How = How.CssSelector, Using = "input[id = 'search-input']")]
+        public IWebElement searchInput;
+
+
+        
+    }
+}
--- UkrNetTests/Tests/LoginTests.cs
+++ UkrNetTests/Tests/LoginTests.cs
@@ -1,16 +1,44 @@
-﻿
-
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using UkrNetTests.Pages;
 
 namespace UkrNetTests.Tests
 {
     class LoginTests
     {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Navigate().GoToUrl("https://www.ukr.net/");
+        }
+        
+        [TearDown]
+
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
         [Test]
 
         public void TestMethod1()
         {
-            System.Console.WriteLine("Test OK11");
+            MainPage mainPage = new MainPage(driver);
+            mainPage.searchInput.Click();
+            mainPage.searchInput.Clear();
+            mainPage.searchInput.SendKeys("Test");
+
+            Thread.Sleep(2000);
+
+
         }
 
     }
