9e82f9e82cfd28e572ec64c17ee973a8eb23db69
vadd3012 <vadd3012@gmail.com>
2018-12-07 08:59:13 +0200

    Add project files.

---
+++ UkrNetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UkrNetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UkrNetTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2182ec0b-146f-4c32-bee6-3a7a77304925")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UkrNetTests/Tests/LoginTests.cs
@@ -0,0 +1,17 @@
+﻿
+
+using NUnit.Framework;
+
+namespace UkrNetTests.Tests
+{
+    class LoginTests
+    {
+        [Test]
+
+        public void TestMethod1()
+        {
+            System.Console.WriteLine("Test OK");
+        }
+
+    }
+}
