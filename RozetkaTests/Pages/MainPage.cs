f3bd6ffdc9b298103e6c8821ce0239a36fd34243
vadd3012 <vadd3012@gmail.com>
2018-12-07 15:25:20 +0200

    Add MainPage, ActionTest

---
+++ RozetkaTests/Pages/MainPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace RozetkaTests.Pages
+{
+    public class MainPage
+    {
+        public MainPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How = How.CssSelector, Using = "div.menu-outer button.menu-toggler .menu-toggler__text")]
+        public IWebElement catalogButton;
+
+        [FindsBy(How = How.CssSelector, Using = "a[href = 'https://rozetka.com.ua/notebooks/asus/c80004/v004/']")]
+        public IWebElement asusCatalogMenuLink;
+
+
+
+        
+    }
+
+}
---
+++ RozetkaTests/Tests/ActionTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using RozetkaTests.Pages;
+using System;
+using System.Threading;
+
+namespace RozetkaTests.Tests
+{
+    class ActionTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Navigate().GoToUrl("https://rozetka.com.ua/");
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+
+        public void LinkAsus ()
+        {
+            Actions actions = new Actions(driver);
+            MainPage mainPage = new MainPage(driver);
+
+            actions.Click(mainPage.catalogButton);
+            Thread.Sleep(2000);
+            actions.Click(mainPage.asusCatalogMenuLink);
+
+
+
+            Thread.Sleep(6000);
+
+
+        }
+        
+
+
+    }
+}
