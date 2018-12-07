c35aee057644dea559e8812926884dd86c1d94c2
vadd3012 <vadd3012@gmail.com>
2018-12-07 09:10:17 +0200

    Add Framework

---
+++ UkrNetTests/Pages/MaiPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace UkrNetTests.Pages
+{
+    class MaiPage
+    {
+        public MaiPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);            
+        }
+        [FindsBy(How = How.CssSelector, Using = "#id-input-login")]
+        public IWebElement loginInput;
+    }
+}
